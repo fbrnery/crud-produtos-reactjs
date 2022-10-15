301a783a8569abcfd120ea346dbebfc6a1ca0159
Fabricio Miranda <fbrnery@gmail.com>
2022-10-15 15:40:08 -0300

    Correção no  Navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -30,45 +30,13 @@ function Navbar() {
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">
-                  Features
+                  Cadastro 
                 </a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">
-                  Pricing
+                  Consulta
                 </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  About
-                </a>
-              </li>
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  data-bs-toggle="dropdown"
-                  href="#"
-                  role="button"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <div class="dropdown-menu">
-                  <a class="dropdown-item" href="#">
-                    Action
-                  </a>
-                  <a class="dropdown-item" href="#">
-                    Another action
-                  </a>
-                  <a class="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                  <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="#">
-                    Separated link
-                  </a>
-                </div>
               </li>
             </ul>
           </div>
